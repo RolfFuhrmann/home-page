039ab2d507c3bcef59a57d8cff410fb9e4847599
RolfFuhrmann <privat@rolf-fuhrmann.de>
2021-01-02 17:10:43 +0100

    Scroll up button

--- libs/index.js
+++ libs/index.js
@@ -24,6 +24,26 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  const scrollToTopbutton = document.getElementById("scrollToTop");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopbutton.style.display = "block";
+  } else {
+    scrollToTopbutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 function ready(handler) {
   if (
     /complete|loaded|interactive/.test(document.readyState) &&
